ca12eaa3149503d0d42043eca5e02b074693dde1
shadrackkiprotich <mylesshadrack99@gmail.com>
2020-12-16 16:56:42 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Employee_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your overtime hours worked?");
+            int number =Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("Number of hours Absent from work");
+            int  hoursabsent = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine ($"Employee  {name} has worked for {number } overtime hours. " +
+                $"The Number of hours absent from work is {hoursabsent} ");
+            Console.ReadKey();
+
+        }
+    }
+}
